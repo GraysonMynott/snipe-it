15bb9f9e4b9851b91bd6687740914a463e59bdde
Grayson Mynott <grayson.mynott@finao.co.nz>
2024-11-04 13:33:00 +1300

    Debugging

--- resources/views/partials/forms/edit/mac-address.blade.php
+++ resources/views/partials/forms/edit/mac-address.blade.php
@@ -3,6 +3,6 @@
     <label for="{{ $fieldname }}" class="col-md-3 control-label">{{ trans('admin/assets/form.mac_address') }} </label>
     <div class="col-md-7 col-sm-12{{  (Helper::checkIfRequired($item, 'mac_address')) ? ' required' : '' }}">
         <input class="form-control" type="text" name="{{ $fieldname }}" id="{{ $fieldname }}" value="{{ old((isset($old_val_name) ? $old_val_name : $fieldname), $item->mac_address) }}" />
-        {!! $errors->first('mac_address', '<span class="alert-msg" aria-hidden="true"><i class="fas fa-times" aria-hidden="true"></i> :message</span>') !!}
+        {!! $errors->all('mac_address', '<span class="alert-msg" aria-hidden="true"><i class="fas fa-times" aria-hidden="true"></i> :message</span>') !!}
     </div>
 </div>
