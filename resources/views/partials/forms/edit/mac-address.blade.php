ebfa5d43a6154e8f093f1dd5afd64665d8a4593e
Grayson Mynott <grayson.mynott@finao.co.nz>
2024-11-04 13:41:01 +1300

    MAC address

--- resources/views/partials/forms/edit/mac-address.blade.php
+++ resources/views/partials/forms/edit/mac-address.blade.php
@@ -4,8 +4,5 @@
     <div class="col-md-7 col-sm-12{{  (Helper::checkIfRequired($item, 'mac_address')) ? ' required' : '' }}">
         <input class="form-control" type="text" name="{{ $fieldname }}" id="{{ $fieldname }}" value="{{ old((isset($old_val_name) ? $old_val_name : $fieldname), $item->mac_address) }}" />
         {!! $errors->first('mac_address', '<span class="alert-msg" aria-hidden="true"><i class="fas fa-times" aria-hidden="true"></i> :message</span>') !!}
-        @foreach ($errors->get('mac_address') as $error)
-                <p>{{ $error }}</p>
-        @endforeach
     </div>
 </div>
